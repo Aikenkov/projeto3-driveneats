d37136d4b518584178a26ad822155b87ba078529
Fabio <Fabio.rod.silva.sqpr@gmail.com>
2022-06-11 13:33:07 -0300

    Habilita o botão ao selecionar 3 itens

--- scripts.js
+++ scripts.js
@@ -1,29 +1,49 @@
+let selectedFood;
+let selectedDessert;
+let selectedDrink;
+
 function selectFood(elemento) {
+    selectedFood = elemento;
     const opcaoSelecionada = document.querySelector('.food-options .selected');
     if (opcaoSelecionada !== null) {
         opcaoSelecionada.classList.remove('selected');
     }
 
     elemento.classList.add('selected');
+    enableOrder()
 }
 
+
+
 function selectDrink(elemento) {
+    selectedDrink = elemento;
     const opcaoSelecionada = document.querySelector('.drink-options .selected');
     if (opcaoSelecionada !== null) {
         opcaoSelecionada.classList.remove('selected');
     }
 
     elemento.classList.add('selected');
+    enableOrder()
 }
 
-let selectedDessert;
+
 
 function selectDessert(elemento) {
-    selectedDessert = elemento.innerHTML;
+    selectedDessert = elemento;
     const opcaoSelecionada = document.querySelector('.dessert-options .selected');
     if (opcaoSelecionada !== null) {
         opcaoSelecionada.classList.remove('selected');
     }
 
     elemento.classList.add('selected');
+    enableOrder()
+}
+
+function enableOrder() {
+    let desabilitado = document.querySelector('.disabled');
+    let habilitado = document.querySelector('.able');
+    if (selectedFood && selectedDrink && selectedDessert) {
+        desabilitado.classList.add('hide');
+        habilitado.classList.remove('hide');
+    }
 }
