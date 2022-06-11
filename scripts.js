fa9ee9dcbcfd91b1cc0054d36e783332fe67e24d
Fabio <Fabio.rod.silva.sqpr@gmail.com>
2022-06-11 15:52:09 -0300

    Encaminhei para o wpp

--- scripts.js
+++ scripts.js
@@ -45,5 +45,11 @@ function enableOrder() {
     if (selectedFood && selectedDrink && selectedDessert) {
         desabilitado.classList.add('hide');
         habilitado.classList.remove('hide');
+        habilitado.innerHTML = `<a href='https://wa.me/5535997290977?text=${meuPedido}'>
+        <h1>Fechar pedido</h1>
+    </a>`;
     }
 }
+
+let strPedido = `Olá, gostaria de fazer o pedido: \n- Prato: Frango Yin Yang \n- Bebida: Coquinha Gelada \n- Sobremesa: Pudim \nTotal: R$ 27.70`;
+let meuPedido = encodeURIComponent(strPedido);
